269b894fd72ac8f1883eb7addf422ff724d90a5b
Nurdaulet Kaishibayev <nkaishibayev@gmail.com>
2021-09-23 19:00:24 -0400

    Finished the fahrenheitToCelcius and added comments + test cases

--- CtoFTester.java
+++ CtoFTester.java
@@ -5,20 +5,36 @@ a double as you're multiplying the input, c, by a fraction.
 
 The function fahrenheitToCelcius will likewise have 1 parameter, which would
 function as the input for Fahrenheit. The parameter will be a double. The return
-also needs to be a double because you are dividing the input, f, by a fraction. 
+also needs to be a double because you are dividing the input, f, by a fraction.
 */
 
 
 public class CtoFTester{
 
-  public static double celsiusToFahrenheit(double c) {
-    double f = (9.0 / 5.0) * c + 32;
+  public static double celsiusToFahrenheit(double c) { // Input Celcius
+    double f = (9.0 / 5.0) * c + 32.0; 
     return f;
+  }
+
+  public static double fahrenheitToCelcius(double f) { // Input Fahrenheit
+    double c = (f - 32.0) / (9.0 / 5.0);
+    return c;
   }
 
 
   public static void main(String[] args) {
+    // Celcius to Fahrenheit
+    System.out.println(celsiusToFahrenheit(1));
+    System.out.println(celsiusToFahrenheit(0));
+    System.out.println(celsiusToFahrenheit(32));
     System.out.println(celsiusToFahrenheit(100));
+    System.out.println(celsiusToFahrenheit(-25));
+    // Fahrenheit to Celcius
+    System.out.println(fahrenheitToCelcius(1));
+    System.out.println(fahrenheitToCelcius(0));
+    System.out.println(fahrenheitToCelcius(212));
+    System.out.println(fahrenheitToCelcius(100));
+    System.out.println(fahrenheitToCelcius(-17));
   }
 }
 
